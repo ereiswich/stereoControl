cb37d9bbe3b148e6e747cd27db7a5d7546c3020f
Eugen Reiswich <er@wps.de>
2018-04-08 19:37:39 +0200

    alten stand gelöscht

--- stereo-control/src/main/java/de/reiswich/homeautomation/stereo_control/avr/IStopPlayingRadioObserver.java
+++
@@ -1,7 +0,0 @@
-package de.reiswich.homeautomation.stereo_control.avr;
-
-public interface IStopPlayingRadioObserver {
-
-	void radioPlayingStopped();
-
-}
--- stereo-control/src/main/java/de/reiswich/homeautomation/stereo_control/scanning/IPhoneObserver.java
+++
@@ -1,8 +0,0 @@
-package de.reiswich.homeautomation.stereo_control.scanning;
-
-public interface IPhoneObserver {
-
-	void iPhoneDetected();
-	
-	void iPhoneOffline();
-}
